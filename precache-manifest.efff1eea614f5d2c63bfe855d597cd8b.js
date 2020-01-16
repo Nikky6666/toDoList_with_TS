46e05e2ae6d4a6f0b7643b705893df03bd1b8c1b
Nikky6666 <borbut.nika@mail.ru>
2020-01-16 21:34:09 +0300

    Updates

---
+++ precache-manifest.efff1eea614f5d2c63bfe855d597cd8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7593de8a98534f65e848fd0065de0909",
+    "url": "/toDoList_with_TS/index.html"
+  },
+  {
+    "revision": "ccc95a97b281bb0d0c1c",
+    "url": "/toDoList_with_TS/static/css/main.0e60bf4b.chunk.css"
+  },
+  {
+    "revision": "e4201756e74b5e330887",
+    "url": "/toDoList_with_TS/static/js/2.7ef6800b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/toDoList_with_TS/static/js/2.7ef6800b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccc95a97b281bb0d0c1c",
+    "url": "/toDoList_with_TS/static/js/main.ba295661.chunk.js"
+  },
+  {
+    "revision": "ab80c19dbaeb8ebd5455",
+    "url": "/toDoList_with_TS/static/js/runtime-main.9c0096c0.js"
+  }
+]);
